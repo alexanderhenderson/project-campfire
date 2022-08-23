c133eea62b58fcb0c7c9e09f6c082556d6e79dea
James <jaems.egan@gmail.com>
2022-08-22 18:45:45 -0600

    added models to events, urls, vies, and encoders

--- backend/events/events_app/models.py
+++ backend/events/events_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    name = models.CharField(max_length=100)
+    # activity = models.ForeignKey(ActivityVO, related_name="activities", on_delete=models.PROTECT)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    description= models.TextField(null=False, blank=False)
+    # owner = models.ForeignKey(UserVO, blank=False, null=False)
+    attendees=models.ManyToManyField("User",blank=True)
+
+    def __str__(self):
+        return f"Event name: {self.name}"
--- backend/events/events_app/views.py
+++ backend/events/events_app/views.py
@@ -1,3 +1,54 @@
+from multiprocessing import Event
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from .models import Events
+from django.http import JsonResponse
+import json
+from common.json import ModelEncoder
+
+class EventEncoder(ModelEncoder):
+    model = Events
+    properties = [
+        "name",
+        "location",
+        "start_date",
+        "end_date",
+        "description",
+        "attendees",
+    ]
+    # encoders = {
+    #     "activity": ActivityEncoder(),
+    #     "owner": LocationEncoder()
+    #     }
+
 
 # Create your views here.
+@require_http_methods(["GET", "POST"])
+def list_all_events(request):
+    if request.method == "GET":
+        events = Events.objects.all()
+        return JsonResponse(
+            {"events": events},
+            encoder=EventEncoder,
+            safe=False,
+        )
+    #POST        
+    # else:
+    #     content = json.loads(request.body) 
+    #     try:
+    #         employee_number = content["technician"]
+    #         technician = Technician.objects.get(employee_number=employee_number)
+    #         content["technician"] = technician
+    #     except Technician.DoesNotExist:
+    #         return JsonResponse(
+    #             {"message": "Not a Valid Employee Number"},
+    #             status=400,
+    #         )
+
+    #     event = Events.objects.create(**content)
+       
+    #     return JsonResponse(
+    #         event,
+    #         encoder=EventEncoder,
+    #         safe=False,
+    #     )
