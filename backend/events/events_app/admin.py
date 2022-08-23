31d105d8b8f87226a8ee18d00c09a72cfabf1db8
Joyce <j8ycey@gmail.com>
2022-08-23 15:02:58 -0700

    created models and updated admin.py

--- backend/events/events_app/admin.py
+++ backend/events/events_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import UserVO, Activity, Event
 
 # Register your models here.
+
+class UserVOAdmin(admin.ModelAdmin):
+    pass
+
+class ActivityAdmin(admin.ModelAdmin):
+    pass
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserVO, UserVOAdmin)
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Event, EventAdmin)
--- backend/events/events_app/models.py
+++ backend/events/events_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class UserVO(models.Model):
+    id = models.PositiveBigIntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'User: {self.id} -{self.name}'
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=300)
+  
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.Charfield(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    owner = models.ForeignKey("User", blank=False, null=False)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    attendees = models.ManyToManyField(UserVO, null=True, blank=True)
+
+    def __str__(self):
+        return f'Event Name: {self.name}'
