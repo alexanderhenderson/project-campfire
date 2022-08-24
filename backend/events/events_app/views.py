332bbb9c4ba9e9db5b73ffd7882cf44180553577
Alexander Henderson <alex.henderson92@yahoo.com>
2022-08-24 14:15:17 -0700

    Event list completed

--- backend/events/events_app/views.py
+++ backend/events/events_app/views.py
@@ -72,91 +72,47 @@ class EventEncoder(ModelEncoder):
 @require_http_methods(["GET", "POST"])
 def list_all_events(request):
     if request.method == "GET":
-        events = Event.objects.all()
-        #events = Event.objects.defer("attendees")
         
-        print("Original queryset: ", events.values())
+        # complete queryset including manyrelationmanager 
+        events = Event.objects.all()
 
-        dict_events = events.defer("attendees").values()
+        # "dictionary" queryset ignoring the attendees / manyrelationmanager
+        #dict_events = Event.objects.all().defer("attendees").values()
+        dict_events = Event.objects.all().values()
+        
+        dict_events = Event.objects.all().values()
+
+        print("query: ", events)
+        print("attendees query: ", events[0].attendees)
+        print("attendees query type: ", type(events[0].attendees))
+        print("dict query: ", dict_events)
+
         result = []
+        
+        # iterating trough a .values() queryset returns plain dictionary
+        # objects
         for i in range(len(dict_events)):
-            print("Loop #: ", i)
-            print("Events dictionary loop: ", dict_events[i])
-            #result[dict_events[i]["name"]] = dict_events[i]
 
+            # the plain dictionary returned from dict_events[i]
+            # is stored in a holding variable
             event_hold = dict_events[i]
-            
             dict_hold = []
-            for dic in events[i].attendees.all().values():
-                print("In attendees loop, adding dic: ", dic)
-                dict_hold.append(dic)
-            print("Attendees dict array: ", dict_hold)
 
+            # looping through attendees dictionary values and
+            # adding them to a holding value
+            for dic in events[i].attendees.all().values():
+                dict_hold.append(dic)
+                print("dictionary: ", dic)
+
+            print("Attendees object dict: ", events[0].attendees.all().values())
+
+            # adding "attendees" key and setting value equal to
+            # list of attendees dictionary key-value pairs
             event_hold["attendees"] = dict_hold
             
+            # adding current event dictionary to result list
             result.append(event_hold)
 
-        for dic in events[0].attendees.all().values():
-            print("Attendees dictionary loop: ", dic)
-
-        print("Result dictionary: ", result)
-
-        print("Events 0: ", events[0], type(events[0]))
-
-        eventsList = list(events)
-
-        for event in eventsList:
-            print("Event list loop: ", event)
-            print("Event list loop: ", event.activity)
-
-        
-        print("Attendees queryset: ", events[0].attendees.all())
-
-        print("Attendees: ", events[0].attendees)
-
-        print("length of events: ", len(events))
-
-        events2 = Event.objects.defer("attendees")
-        print("Events 2 is type: ", type(events2[0]))
-
-        full_queryset = []
-
-        for i in range(len(events)):
-            print("testing for loop")
-            if i == 0:
-                #print("Before failing events2[i] is type: ", type(events2[i].all()))
-                full_attendees_queryset = events[i].attendees.all()
-                #test = events2[i].name | events[i].attendees.all()
-
-            else:
-                full_attendees_queryset = full_attendees_queryset | events[i].attendees.all()
-                #test = events2[i].name | events[i].attendees.all()
-
-            
-        print("full_attendees_queryset type: ", type(full_attendees_queryset), "full queryset: ", full_attendees_queryset)
-            
-
-        for x in events[0].attendees.all():
-            print("in for loop")
-            print("User name is ", x.name, " and user ID is: ", x.id)
-            print("X is type: ", type(x))
-
-        attendees = events[0].attendees.all()
-
-
-        # defer allows you to create a queryset that ignores a model field
-        print("Test line below this might fail")
-        print(Event.objects.defer("attendees"))
-        print("It didn't fail")
-
-        print(events[0].attendees)
-        print("Get request - All events:", events._fields, " Type: ", type(events[0].attendees))
-
-        events2 = Event.objects.defer("attendees")
-        # events2 = Event.objects.only("name")
-
-        print("below should fail")
-        print(events2[0].__dict__)
         return JsonResponse(
             {"Events": result},
             encoder=EventEncoder,
