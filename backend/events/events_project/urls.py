bd9fe43f3a42fb8eeb397a677e394eed64fbc6b5
James <jaems.egan@gmail.com>
2022-08-23 19:13:23 -0600

    views and urls, committing to save

--- backend/events/events_project/urls.py
+++ backend/events/events_project/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("events/", include("events_app.urls"))
+    path("events/", include("events_app.urls")),
     path("", include("djwto.urls")),
 ]
