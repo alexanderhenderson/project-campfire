7763d74e76f3dea6cde952c141f0279b1663c164
Joyce <j8ycey@gmail.com>
2022-08-24 18:26:49 -0700

    urls were missing, added those

--- backend/events/events_app/views.py
+++ backend/events/events_app/views.py
@@ -1,5 +1,3 @@
-<<<<<<< backend/events/events_app/views.py
-=======
 import json
 
 from common.json import ModelEncoder
@@ -10,7 +8,6 @@ from django.views.decorators.http import require_http_methods
 from .models import Activity, Event, UserVO
 
 
-<<<<<<< backend/events/events_app/views.py
 class UserVOEncoder(ModelEncoder):
     model = UserVO
     properties = [
@@ -57,51 +54,6 @@ def list_all_events(request):
 @require_http_methods(["GET"])
 def list_users_events(request):
     pass
-=======
-# class EventEncoder(ModelEncoder):
-#     model = Event
-#     properties = [
-#         "name",
-#         "activity",
-#         "latitude",
-#         "longitude",
-#         "start_date",
-#         "end_date",
-#         "description",
-#         "owner",
-#         "attendees",
-#     ]
-#     # encoders = {
-#     #     "activity": ActivityEncoder(),
-#     #     "owner": LocationEncoder()
-#     #     }
-
-
-# # Create your views here.
-# @require_http_methods(["GET", "POST"])
-# def list_all_events(request):
-#     if request.method == "GET":
-#         events = Event.objects.all()
-#         return JsonResponse(
-#             {"events": events},
-#             encoder=EventEncoder,
-#             safe=False,
-#         )
-    #POST        
-    # else:
-    #     content = json.loads(request.body) 
-    #     try:
-    #         employee_number = content["technician"]
-    #         technician = Technician.objects.get(employee_number=employee_number)
-    #         content["technician"] = technician
-    #     except Technician.DoesNotExist:
-    #         return JsonResponse(
-    #             {"message": "Not a Valid Employee Number"},
-    #             status=400,
-    #         )
-
-    #     event = Events.objects.create(**content)
->>>>>>> backend/events/events_app/views.py
 
 @require_http_methods(["GET"])
 def list_event_detail(request):
