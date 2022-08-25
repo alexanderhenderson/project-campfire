55c12a46c92081ee412dd0a7f36235bc69d7934a
Alexander Henderson <alex.henderson92@yahoo.com>
2022-08-24 17:34:19 -0700

    ManyRelationManager Defeated

--- backend/events/common/json.py
+++ backend/events/common/json.py
@@ -3,19 +3,20 @@ from django.urls import NoReverseMatch
 from django.db.models import QuerySet, ManyToManyField
 from datetime import datetime, time
 import decimal
-import json
 
-class DecimalEncoder(json.JSONEncoder):
+class DecimalEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
             return str(o)
-        return super(DecimalEncoder, self).default(o)
+        return super().default(o)
+        #return super(DecimalEncoder, self).default(o)
 
 class DateEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime) or isinstance(o, time):
             return o.isoformat()
         else:
+            print("Printstop 9")
             return super().default(o)
 
 
@@ -24,6 +25,8 @@ class QuerySetEncoder(JSONEncoder):
         if isinstance(o, QuerySet):
             return list(o)
         else:
+            print("Printstop 10", type(self), self)
+            print("Printstop 11", type(o), o)
             return super().default(o)
 
 # class ManyToManyEncoder(JSONEncoder):
@@ -36,17 +39,20 @@ class QuerySetEncoder(JSONEncoder):
 
 class ManyRelatedManagerEncoder(JSONEncoder):
     def default(self, o):
-        if type(o) == 'ManyRelatedManager':
+        if hasattr(o, "all"):
             print("test passes")
-            return list(o)
+            #return list(o.all())
+            print(type(list(o.all())))
+            return list(o.all())
         else:
             return super().default(o)
 
-class ModelEncoder(DateEncoder, DecimalEncoder, QuerySetEncoder, ManyRelatedManagerEncoder, JSONEncoder):
+class ModelEncoder(DateEncoder, QuerySetEncoder, DecimalEncoder, JSONEncoder):
     encoders = {}
 
     def default(self, o):
         if isinstance(o, self.model):
+            print("ModelEncoder")
             d = {}
             if hasattr(o, "get_api_url"):
                 try:
@@ -54,9 +60,15 @@ class ModelEncoder(DateEncoder, DecimalEncoder, QuerySetEncoder, ManyRelatedMana
                 except NoReverseMatch:
                     pass
             for property in self.properties:
+                encoder = self.encoders.get(property)
                 value = getattr(o, property)
-                if property in self.encoders:
-                    encoder = self.encoders[property]
+                if hasattr(value, "all") and callable(value.all):
+                    value = map(
+                        encoder.default if encoder else lambda x: x,
+                        list(value.all()),
+                    )
+                    value = list(value)
+                elif encoder:
                     value = encoder.default(value)
                 d[property] = value
             d.update(self.get_extra_data(o))
@@ -66,3 +78,43 @@ class ModelEncoder(DateEncoder, DecimalEncoder, QuerySetEncoder, ManyRelatedMana
 
     def get_extra_data(self, o):
         return {}
+
+
+
+# class ModelEncoder(QuerySetEncoder, ManyRelatedManagerEncoder, DateEncoder, DecimalEncoder, JSONEncoder):
+#     encoders = {}
+
+#     def default(self, o):
+#         print("Printstop 1", type(self), type(o))
+#         if isinstance(o, self.model):
+#             print("Printstop 2")
+#             d = {}
+#             if hasattr(o, "get_api_url"):
+#                 print("Printstop 3")
+#                 try:
+#                     d["href"] = o.get_api_url()
+#                     print("Printstop 4")
+
+#                 except NoReverseMatch:
+#                     print("Printstop 5")
+#                     pass
+#             for property in self.properties:
+#                 print("Printstop 6")
+
+#                 value = getattr(o, property)
+#                 if property in self.encoders:
+#                     print("Printstop 7 - property: ", property)
+#                     print("Value type: ", type(value), " value: ", value)
+#                     encoder = self.encoders[property]
+#                     value = encoder.default(value)
+#                     print("Return from nested default call")
+#                 d[property] = value
+#             d.update(self.get_extra_data(o))
+#             return d
+#         else:
+#             print("Printstop 8")
+#             return super().default(o)
+
+#     def get_extra_data(self, o):
+#         return {}
+
--- backend/events/events_app/views.py
+++ backend/events/events_app/views.py
@@ -11,6 +11,16 @@ class UserVOEncoder(ModelEncoder):
         "name"
     ]
 
+    def default(self, o):
+
+        print("UserVO Default called", type(o))
+        a = super().default(o)
+        print("End user default call: ", a)
+        return a
+
+
+
+
 class ActivityEncoder(ModelEncoder):
     model = Activity
     properties = [
@@ -43,29 +53,6 @@ class EventEncoder(ModelEncoder):
     #         "attendees": o.attendees_set.all(),
     #         }
 
-# class SaleEncoder(ModelEncoder):
-#     model = Sale
-#     properties = [
-#         "id",
-#         "price",
-#         "salesrep",
-        
-#         "customer",
-#         "automobile",
-#     ]
-#     encoders = {
-#         "salesrep": SalesRepEncoder(),
-#         "customer": CustomerEncoder(),
-#         "automobile": AutomobileVOEncoder(),
-#     }
-
-    # def get_extra_data(self, o):
-    #     return {
-    #         "salesrep": o.salesrep.name,
-    #         "employeenumber": o.salesrep.employeenumber,
-    #         "customer": o.customer.name,
-    #         "automobile": o.automobile.vin,
-    #         }
 
 
 # # Create your views here.
@@ -76,45 +63,48 @@ def list_all_events(request):
         # complete queryset including manyrelationmanager 
         events = Event.objects.all()
 
-        # "dictionary" queryset ignoring the attendees / manyrelationmanager
-        #dict_events = Event.objects.all().defer("attendees").values()
-        dict_events = Event.objects.all().values()
+        # # "dictionary" queryset ignoring the attendees / manyrelationmanager
+        # #dict_events = Event.objects.all().defer("attendees").values()
+        # dict_events = Event.objects.all().values()
         
-        dict_events = Event.objects.all().values()
 
-        print("query: ", events)
-        print("attendees query: ", events[0].attendees)
-        print("attendees query type: ", type(events[0].attendees))
-        print("dict query: ", dict_events)
 
-        result = []
+        # dict_events = Event.objects.all().values()
+
+        # print("query: ", events)
+        # print("attendees query: ", events[0].attendees().all())
+        # print("attendees query type: ", type(events[0].attendees))
+        # print("dict query: ", dict_events)
+        # print("dict query: ", type(dict_events))
+
+        # result = []
         
         # iterating trough a .values() queryset returns plain dictionary
         # objects
-        for i in range(len(dict_events)):
+        # for i in range(len(dict_events)):
 
-            # the plain dictionary returned from dict_events[i]
-            # is stored in a holding variable
-            event_hold = dict_events[i]
-            dict_hold = []
+        #     # the plain dictionary returned from dict_events[i]
+        #     # is stored in a holding variable
+        #     event_hold = dict_events[i]
+        #     dict_hold = []
 
-            # looping through attendees dictionary values and
-            # adding them to a holding value
-            for dic in events[i].attendees.all().values():
-                dict_hold.append(dic)
-                print("dictionary: ", dic)
+        #     # looping through attendees dictionary values and
+        #     # adding them to a holding value
+        #     for dic in events[i].attendees.all().values():
+        #         dict_hold.append(dic)
+        #         print("dictionary: ", dic)
 
-            print("Attendees object dict: ", events[0].attendees.all().values())
+        #     print("Attendees object dict: ", events[0].attendees.all().values())
 
-            # adding "attendees" key and setting value equal to
-            # list of attendees dictionary key-value pairs
-            event_hold["attendees"] = dict_hold
+        #     # adding "attendees" key and setting value equal to
+        #     # list of attendees dictionary key-value pairs
+        #     event_hold["attendees"] = dict_hold
             
-            # adding current event dictionary to result list
-            result.append(event_hold)
+        #     # adding current event dictionary to result list
+        #     result.append(event_hold)
 
         return JsonResponse(
-            {"Events": result},
+            {"Events": events},
             encoder=EventEncoder,
             safe=False,
         )
