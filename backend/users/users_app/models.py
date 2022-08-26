5ef087328abce122d4c946dcc828ab03565f7a14
Totz96 <lotztim@gmail.com>
2022-08-25 22:52:14 -0400

    completed the views for users microservice

--- backend/users/users_app/models.py
+++ backend/users/users_app/models.py
@@ -10,22 +10,25 @@ from django.contrib.auth.models import AbstractUser
 # User = settings.AUTH_USER_MODEL
 
 class User(AbstractUser):
-    name = models.CharField(max_length=100)
+    # name = models.CharField(max_length=100)
     #friend = models.ManyToManyField("user", blank=True)
-    profile_description = models.TextField()
+    profile_description = models.TextField(null=True, blank=True)
     
     # stretch goal - store in mongo
-    profile_photo = models.URLField()
-    email = models.EmailField()
+    profile_photo = models.URLField(null=True, blank=True)
+    # email = models.EmailField()
 
     # front end may end up filtering this
     city = models.CharField(max_length=150)
     state = models.CharField(max_length=2)
     #favorite_activities = models.ManyToManyField("ActivityVO")
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return self.name
+        return self.username
 
 class ActivityVO(models.Model):
     name = models.CharField(max_length=300)
