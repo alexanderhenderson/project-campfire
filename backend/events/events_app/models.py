39efc933d2974a151872786ee902d7ffd67b37bd
Alexander Henderson <alex.henderson92@yahoo.com>
2022-08-29 14:40:55 -0700

    Event Activity POST fixed

--- backend/events/events_app/models.py
+++ backend/events/events_app/models.py
@@ -8,10 +8,9 @@ class UserVO(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.username}"
 
 class Activity(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=300)
 
     def __str__(self):
