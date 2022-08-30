c50a697e9c352eea93bed1e8ba4968ca9be23e30
Totz96 <lotztim@gmail.com>
2022-08-30 19:37:07 -0400

    used useEffect to get the user details

--- frontend-app/src/UserProfile.js
+++ frontend-app/src/UserProfile.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react"
 
 
 function UserProfile() {
+    const [userData, setUserData] = useState({});
+    const [error, setError] = useState('');
+  
+    useEffect(() => {
+        const getUserData = async () => {
+            const url = `${process.env.REACT_APP_USERS}/users/1/`;
+            const response = await fetch(url);
+            if (response.ok) {
+                // console.log("response: ", response)
+                const data = await response.json();
+                // console.log("data: ", data)
+                setUserData(data);
+            } else {
+                setError('Could not load the things');
+            }
+        }
+        getUserData()
+    }, [setUserData, setError])
+    console.log(userData)
+
     return (
         <div>
             Hello World
