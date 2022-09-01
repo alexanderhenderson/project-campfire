3a4eb3f37c926f89fc02cd764be1a525ec3d6270
James <jaems.egan@gmail.com>
2022-08-31 18:24:02 -0600

    dumb

--- frontend-app/src/login.js
+++ frontend-app/src/login.js
@@ -1,54 +1,43 @@
-import React from 'react';
-import { useState } from 'react';
-import { useToken } from './Authorization';
+// import './App.css';
+import React,{useState} from 'react';
+import { useToken } from './auth';
+import { useParams } from 'react-router-dom';
 
 
-export default function LogIn() {
+function Login() {
+const [data,setData] = useState({
+username:"",
+password:""
+})
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [token, login] = useToken();
-    const [loginResponse, setLoginResponse] = useState();
+const {username,password} = data;
+const {id} = useParams()
 
-    async function onSubmit(){
+const [token,login] = useToken();
 
-      //console.log("Submitted, token: ", token)
-
-      //console.log("function: ", login);
-      
-      const result = await login(username, password);
-      setLoginResponse(result);
-
-      if (result == null) {
-        setUsername("");
-        setPassword("");
-      };
-
-    }
-
-  return(
-    <div className="container px-4 py-4 text-center">
-      <div className="row gx-5">
-        <div className="col">
-          <div className="card shadow">
-            <div className = "card body px-4 py-4">
-              <form>
-                <h1> Log in </h1>
-                <div className="form-floating mb-3">
-                  <input type="text" value={username} onChange={e => setUsername(e.target.value)} id="username" className="form-control"/>
-                  <label htmlFor="username"> Username </label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input type="password" value={password} onChange={e => setPassword(e.target.value)} id="password" className="form-control"/>
-                  <label htmlFor="password"> Password </label>
-                </div>
-                <button type="button" className="btn btn-success" onClick={onSubmit}>Log in</button>
-              </form>
-              <p className="fs-5" hidden={ (loginResponse !== undefined) ? false : true }> {loginResponse} </p>              
-              </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const changeHandler = e => {
+setData({...data,[e.target.name]:[e.target.value]});
 }
+console.log(data)
+
+const submitHandler = e => {
+e.preventDefault();
+login(data.username[0], data.password[0],{id}.id)
+console.log(data);
+}
+return (
+    <div className="wrapper fadeInDown">
+        <div id="formContent">
+            <div className="fadeIn first">
+            </div>
+            <form>
+                <input type="text" id="username" className="fadeIn second" name="login" placeholder="Enter Username" />
+                <input type="text" id="password" className="fadeIn third" name="login" placeholder="Enter Password" />
+                <input type="submit" className="fadeIn fourth" value="Sign In" />
+            </form>
+        </div>
+    </div>
+);
+}
+
+export default Login;
