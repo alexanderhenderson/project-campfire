fdad33cf0d54f9e4a9560842eaf489c9946a3852
Joyce <j8ycey@gmail.com>
2022-09-02 11:24:51 -0700

    updated homepage, will merge main into this branch before finishing it up to test registration since my button will need to be linked to it

--- frontend-app/src/MainHomepage.js
+++ frontend-app/src/MainHomepage.js
@@ -3,16 +3,10 @@ import EventCard from "./Components/EventCard";
 function MainPage() {
   return (
     <>
-      <img className="mw-100 mt-3 mb-3" src="/mountain-homepage.png" alt="Max-width 100%"></img>
-      <div className="px-4 py-5 my-5 text-center">
-        <h1 className="display-5 fw-bold">Campfire</h1>
-        <div className="col-lg-6 mx-auto">
-          <div className="lead mb-4">
-            Gather around the campfire with new friends
-          </div>
-        </div>
-        {/* <div><small>Find your tribe</small></div> */}
-      <EventCard />
+      <img className="mw-100 mt-3" src="/cf-logo.png" alt="Max-width 100%"></img>
+      <div className="px-4 py-5 my-5 text-center mb-3">
+        <h2>Gather around the campfire with new friends</h2>
+        <EventCard />
       </div>
     </>
   );
