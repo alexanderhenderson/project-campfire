beecab08f2ce6c6201dbdaa79794c793ed69c738
Joyce L <j8ycey@gmail.com>
2022-09-06 22:50:58 -0700

    updated events/detail/ page, still need to make urls dynamic with event id

--- frontend-app/src/EventDetail.js
+++ frontend-app/src/EventDetail.js
@@ -23,12 +23,25 @@ function FetchEvent() {
 
     return (
         <>
-            <div className='m-3'>
-                <h1 className='display-4'> {Events[0]?.name || ''}</h1>
-                {<img src={Events[0]?.picture_url} className='img-fluid max-width: 100%' />}
-                <p className='lead'> Description: {Events[0]?.description || ''}</p>
-                <p className='lead'> Activity: {Events[0]?.activity.name || ''}</p>
-                <p className='lead'>Date: {new Date(Events[0]?.start).toLocaleString()} - {new Date(Events[0]?.end).toLocaleString()}</p>
+            <div className="container px-4 py-4">
+                <div className="row gx-5">
+                    <div className="col">
+                        <div className="card shadow">
+                            <div className="card body px-4 py-4">
+                                <h1 className='display-4 text-center'> {Events[0]?.name || ''}</h1>
+                                {<img src={Events[0]?.picture_url} className='img-fluid max-width: 100%' />}
+                                <span className='mt-3'>
+                                    <h2 className='display-6'>Description</h2> 
+                                    <p className='lead text-left'>{Events[0]?.description || ''}</p>
+                                    <h3 className='display-6'>Activity</h3>
+                                    <p className='lead'>{Events[0]?.activity.name || ''}</p>
+                                    <h3 className='display-6'>Dates</h3>
+                                    <p className='lead'>{new Date(Events[0]?.start).toLocaleString()} - {new Date(Events[0]?.end).toLocaleString()}</p>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
