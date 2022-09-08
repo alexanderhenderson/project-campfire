b0613076ed5f018215c4107519e1e199e35bb910
Totz96 <lotztim@gmail.com>
2022-09-08 18:41:40 -0400

    linting users

--- backend/users/users_app/models.py
+++ backend/users/users_app/models.py
@@ -1,6 +1,4 @@
-from typing_extensions import Required
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 
 
@@ -9,23 +7,21 @@ class ActivityVO(models.Model):
     name = models.CharField(max_length=300)
     id = models.IntegerField(primary_key=True)
 
+
 class User(AbstractUser):
-    # name = models.CharField(max_length=100)
     friends = models.ManyToManyField("self", blank=True)
     profile_description = models.TextField(null=True, blank=True)
-    
-    # stretch goal - store in mongo
     profile_photo = models.URLField(null=True, blank=True)
-    # email = models.EmailField()
-
-    # front end may end up filtering this
     city = models.CharField(max_length=150)
     state = models.CharField(max_length=2)
-    favorite_activities = models.ManyToManyField(ActivityVO, blank=True, related_name="activities")
+    favorite_activities = models.ManyToManyField(
+        ActivityVO,
+        blank=True,
+        related_name="activities"
+    )
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.username
--- backend/users/users_app/urls.py
+++ backend/users/users_app/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-
-from django.contrib.auth.views import LoginView, LogoutView
 from .views import (
     list_users,
     user_detail,
@@ -15,11 +13,9 @@ urlpatterns = [
     # JWT web token paths
     path("api/tokens/mine/", api_user_token, name="user_token"),
     path("api/tokens/user/", api_user_info, name="user_info"),
-    
     # other request endpoints
     path("", list_users, name="api_list_users"),
     path("<int:pk>/", user_detail, name="api_user_detail"),
     path("activities/", list_activities, name="api_list_activities"),
     path("activities/<int:pk>/", activity_detail, name="api_activity_details"),
-
 ]
