ac04655c1c7adf3095ae8d1cdd749dc747dd11d4
Joyce L <j8ycey@gmail.com>
2022-09-08 17:33:36 -0700

    updated profile page

--- frontend-app/src/ActivitiesList.js
+++ frontend-app/src/ActivitiesList.js
@@ -1,15 +1,15 @@
-import { useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 import { addActivities } from "./Components/AddActivityToList"
 
 export default function FetchActivities() {
-    
+
     const [activities, setActivitiesData] = useState([])
     const [userData, setUserId] = useState("")
     const [activityList, setActivityList] = useState([])
     const [search, setSearch] = useState('')
     const [filteredActivities, setFilteredActivities] = useState([])
     const [clicked, setClicked] = useState(false)
-  
+
     useEffect(() => {
         //promise Chain cool!
         // const userFetch = fetch (`${process.env.REACT_APP_USERS}/users/api/tokens/user/`, {
@@ -22,10 +22,10 @@ export default function FetchActivities() {
             if (response.ok) {
                 const data = await response.json()
                 setActivitiesData(data["Activities"])
-                
+
             }
         }
-        
+
         const getUserdata = async () => {
             const url = `${process.env.REACT_APP_USERS}/users/api/tokens/user/`;
             const response = await fetch(url, { credentials: "include" });
@@ -35,26 +35,26 @@ export default function FetchActivities() {
                 setActivityList(userData.favorite_activities)
             }
         }
-        
+
         getActivityData()
         getUserdata()
-        
-    },[clicked] )
+
+    }, [clicked])
 
     function searchFilter() {
         const searchedActivities = activities.filter(activity => activity.name.toLowerCase().includes(search.toLowerCase()))
         setFilteredActivities(searchedActivities)
-      }
-      useEffect(() => { searchFilter() }, [search])
-    
-      function handleChange(event) {
+    }
+    useEffect(() => { searchFilter() }, [search])
+
+    function handleChange(event) {
         setSearch(event.target.value)
-      }
+    }
 
     const userFavesIds = activityList.map(act => act.id);
-    const activityState = filteredActivities.length < 1 
-    ? activities 
-    : filteredActivities;
+    const activityState = filteredActivities.length < 1
+        ? activities
+        : filteredActivities;
 
     return (
         <main>
@@ -62,16 +62,16 @@ export default function FetchActivities() {
                 <h1>Activities</h1>
                 <h4>Click to add to your favorite activities!</h4>
             </div>
-                <div>
-                    <input
+            <div>
+                <input
                     type="search"
                     id="search"
                     className="form-control"
                     placeholder="Search for Activities"
                     onChange={handleChange}
                     aria-label="Search"
-                    />
-                </div>
+                />
+            </div>
             <div className="m-3">
                 <div className="row ">
                     {activityState.filter(act => !userFavesIds.includes(act.id))
@@ -82,7 +82,7 @@ export default function FetchActivities() {
                                         <div onClick={() => {
                                             addActivities(userData.id, activity)
                                             setClicked(!clicked)
-                                             }}>
+                                        }}>
                                             <img src={activity.picture_url} className="card-img-top" />
                                             <div className="card-body">
                                                 <h5 className="card-title center_card_text">{activity.name}</h5>
--- frontend-app/src/UserProfile.js
+++ frontend-app/src/UserProfile.js
@@ -1,54 +1,73 @@
 import { useEffect, useState } from "react"
 
 
-function UserProfile() {
+export default function UserProfile() {
     const [userData, setUserData] = useState({})
-    const [error, setError] = useState('')
     const [userId, setUserId] = useState(6)
 
     useEffect(() => {
         const getUserData = async () => {
-            const url = `${process.env.REACT_APP_USERS}/users/${userId}/`;
-            const response = await fetch(url);
+            const url = `${process.env.REACT_APP_USERS}/users/${userId}/`
+            const response = await fetch(url)
             if (response.ok) {
                 // console.log("response: ", response)
-                const data = await response.json();
+                const data = await response.json()
                 // console.log("data: ", data)
-                setUserData(data);
-            } else {
-                setError('Could not load users');
+                setUserData(data)
             }
         }
         getUserData()
-    }, [setUserData, setError])
-    console.log(userData)
+    }, [setUserData])
 
     return (
         <>
+
             <div className="container px-4 py-4">
                 <div className="col">
                     <div className="card shadow">
-                        <div className="body px-4 py-4">
-                            <h1>{userData.first_name} {userData.last_name} : {userData.username}</h1>
-                        </div>
                         <div className="container m-3">
-                            <div className='m-2'>
-                                <img src={userData.profile_photo} height='200' />
+                            <div className="container">
+                                <div className="row">
+                                    <div className="body my-3">
+                                        <h1>{userData.username}</h1>
+                                    </div>
+                                    <div className="col">
+                                        <div className='mb-5'>
+                                            <img src={userData.profile_photo} height='200' />
+                                        </div>
+                                        <div className='m-2'>
+                                            <h4>Name: </h4>
+                                            <p>{userData.first_name} {userData.last_name}</p>
+                                            <h4>About Me: </h4>
+                                            <p>{userData.profile_description}</p>
+                                            <h4>Location</h4>
+                                            <p>{userData.city}, {userData.state}</p>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <h4>Favorite Activities</h4>
+                                        <table className="table">
+                                            <tbody>
+                                                {userData?.favorite_activities?.map(activity => (
+                                                    <tr key={activity.id}>
+                                                        <td>{activity.name}</td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                        <h4>Friends</h4>
+                                        <table className="table">
+                                            <tbody>
+                                                {userData?.friends?.map(friend => (
+                                                    <tr key={friend.id}>
+                                                        <td>{friend.username}</td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
-                            <h4>About Me: </h4>
-                            <p>{userData.profile_description}</p>
-                            <h4>Location</h4>
-                            <p>{userData.city}, {userData.state}</p>
-                            <h4>Friends</h4>
-                            <table className="table">
-                                <tbody>
-                                    {userData?.friends?.map(friend => (
-                                        <tr key={friend.id}>
-                                            <td>{friend.username}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
@@ -57,5 +76,3 @@ function UserProfile() {
         </>
     )
 }
-
-export default UserProfile
