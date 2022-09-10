75cf039dd518fb659f5d75343af03720e94c7b1b
Joyce L <j8ycey@gmail.com>
2022-09-10 00:52:05 -0700

    updated homepage to original mountain photo again. still trying to figure out how to import and use google fonts

--- frontend-app/src/Components/Hero.js
+++ frontend-app/src/Components/Hero.js
@@ -8,18 +8,17 @@ export default function Hero() {
     <>
       <div className='hero'></div>
       <div className='home-div'>
-      <div className="p-5 text-center">
-        <div className="d-flex justify-content-center align-items-center h-500">
-          <div className='text-white'>
-            <h1 className="mb-3 hero-text lead"><strong>Campfire</strong></h1>
-            <h4 className="mb-3 hero-sub-text lead">Come join our campfire</h4>
-            <a className="btn btn-outline-light btn-lg rounded-pill" href="/signup/" role="button">Sign Up</a>
-            <div className='m-5'>
-            <p className="forgot-password text-right lead">
-              Already Signed Up? <a href={`Login`}>Log In</a>
-            </p>
+        <div className="p-5 text-center">
+          <div className="d-flex justify-content-center align-items-center h-500">
+            <div className='text-white'>
+              <h1 className="mb-3 hero-text lead font-weight-bold">Campfire</h1>
+              <h4 className="mb-3 hero-sub-text lead font-weight-bold">Come join our campfire</h4>
+              <div className='hero-bottom'>
+              <a className="btn btn-outline-light btn-lg rounded-pill mb-3" href="/signup/" role="button">Sign Up</a>
+              <p className="forgot-password"> Already Signed Up? <a href={`Login`}>Log In</a>
+              </p>
             </div>
-          </div>
+            </div>
           </div>
         </div>
       </div>
