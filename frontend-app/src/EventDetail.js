def00f3a8a56313ca7648cfbcbe814bae52487e1
James <jaems.egan@gmail.com>
2022-09-12 15:46:33 -0600

    test

--- frontend-app/src/EventDetail.js
+++ frontend-app/src/EventDetail.js
@@ -7,25 +7,25 @@ function FetchEvent() {
     const [error, setError] = useState("")
     const [userData, setUserId] = useState("")
     const [attendeesList, setAttendeesList] = useState([])
-    const {dynamicId} = useParams()
+    const { dynamicId } = useParams()
     const [button, setButton] = useState('btn btn-outline-warning button-font')
-   
-   
+
+
     useEffect(() => {
         const getEventData = async () => {
             const url = `${process.env.REACT_APP_EVENTS}/events/${dynamicId}`
             const response = await fetch(url)
             if (response.ok) {
                 const eventData = await response.json()
-                
+
                 setEventsData(eventData["Event"])
                 setAttendeesList(eventData.Event.attendees)
-                
+
             } else {
                 setError("Could not load the events, try again")
             }
         }
-    
+
 
         const getUserdata = async () => {
             const url = `${process.env.REACT_APP_USERS}/users/api/tokens/user/`;
@@ -39,16 +39,16 @@ function FetchEvent() {
         getEventData()
         getUserdata()
     }, [])
-    
+
     const eventAttendees = Events.attendees
     const currentUser = userData.id
-    
-    function clickHandler(){     
+
+    function clickHandler() {
         addAttendee(userData.id, dynamicId)
         setButton('btn btn-outline-warning button-font d-none')
         setAttendeesList([...attendeesList, userData])
-        
-    }  
+
+    }
     return (
         <>
             <div className="container px-4 py-4">
@@ -58,11 +58,11 @@ function FetchEvent() {
                             <div className="card body px-4 py-4">
                                 <h1 className='display-4 text-center'> {Events?.name || ''} </h1>
                                 {<img src={Events?.picture_url} className='img-fluid max-width: 100%' />}
-                                <p> 
-                                    <button onClick={() => { 
-                                    clickHandler()
-                                 }}
-                                 type="button" className={button}>Click to Attend</button>
+                                <p>
+                                    <button onClick={() => {
+                                        clickHandler()
+                                    }}
+                                        type="button" className={button}>Click to Attend</button>
                                 </p>
                                 <span className='mt-3'>
                                     <h2 className='display-6'>Description</h2>
@@ -77,11 +77,11 @@ function FetchEvent() {
                                     <tbody>
                                         {attendeesList.map(attendee => {
                                             return (
-                                            <tr key={attendee.id}>
-                                                <td>{attendee.first_name} {attendee.last_name}</td>
-                                            </tr>
+                                                <tr key={attendee.id}>
+                                                    <td>{attendee.first_name} {attendee.last_name}</td>
+                                                </tr>
                                             )
-                                            
+
                                         })}
                                     </tbody>
                                 </table>
