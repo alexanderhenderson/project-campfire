1f4d815bb9f0aa697abfc369e32eec3d1ea44654
Totz96 <lotztim@gmail.com>
2022-09-12 19:15:52 -0400

    reanaming login and logout pt 1

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -4,8 +4,8 @@ import IntroPage from "./IntroPage"
 import UserHomepage from "./UserHomepage"
 import NavBar from './Nav'
 import UserProfile from './UserProfile'
-import LogIn from './login'
-import LogOut from './logout'
+import LogIn from './login1'
+import LogOut from './logout1'
 import Signup from "./SignUp"
 import EventList from './EventPage'
 import ActivitiesList from './ActivitiesList'
