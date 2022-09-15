5a43ec91a2dc5ce81ac36f760104bd5832dac9ec
Joyce L <j8ycey@gmail.com>
2022-09-15 12:08:39 -0700

    merging w main

--- frontend-app/src/UserProfile.js
+++ frontend-app/src/UserProfile.js
@@ -99,6 +99,7 @@ export default function UserProfile() {
                                                 ))}
                                             </tbody>
                                         </table>
+                                        
                                     </div>
                                 </div>
                             </div>
