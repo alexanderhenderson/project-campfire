a8585dc14635df41eb191725eff7afe61837d599
Joyce L <j8ycey@gmail.com>
2022-09-14 20:53:25 -0700

    updated intro page again and finished this weeks journals

--- frontend-app/src/IntroPage.js
+++ frontend-app/src/IntroPage.js
@@ -7,71 +7,77 @@ export default function IntroPage() {
   return (
     <>
       <div className='intro-bg'></div>
-      <div className="px-4 text-center text-white over-bg padding-top">
+      <div className="px-4 text-center text-black over-bg padding-top">
         <div className="mb-3">
           <h1>New here? Let us show you around.</h1>
         </div>
       </div>
-      <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+      <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" className=""></button>
+          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" className="active" aria-current="true"></button>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item">
-            {/* <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg> */}
+        <div className="carousel-inner">
+          <div className="carousel-item">
+            {/* <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg> */}
             <img
               className="d-block"
               src="https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png"
               height='300'
             />
-            <div class="container">
-              <div class="carousel-caption">
+            <div className="container">
+              <div className="carousel-caption text-black">
                 <h1>Activities</h1>
-                <p>Select some favorite activities so we know what you're into.</p>
-                <p> <a className="btn btn-outline-light btn-lg rounded-pill mb-3" href="/activities/" role="button">See All Activities</a></p>
+                <h3>Let us know what you're into.</h3>
+                <div className='mt-4'>
+                  <p> <a className="btn btn-outline-dark btn-lg rounded-pill mb-3" href="/activities/" role="button">See All Activities</a></p>
+                </div>
               </div>
             </div>
           </div>
-          <div class="carousel-item">
-            {/* <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg> */}
+          <div className="carousel-item">
+            {/* <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg> */}
             <img
               className="d-block"
               src="https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png"
               height='300'
             />
-            <div class="container">
-              <div class="carousel-caption">
+            <div className="container">
+              <div className="carousel-caption text-black">
                 <h1>Kindler</h1>
-                <p>Meet friends based on common interests in activities. </p>
-                <p><a className="btn btn-outline-light btn-lg rounded-pill mb-3" href="/partnerfinder/" role="button">Kindler: Meet Your Match 🔥</a></p>
+                <div className='mt-4'>
+                  <h3>Meet friends based on common interests. </h3>
+                  <p><a className="btn btn-outline-dark btn-lg rounded-pill mb-3" href="/partnerfinder/" role="button">Kindler: Meet Your Match 🔥</a></p>
+                </div>
               </div>
             </div>
           </div>
-          <div class="carousel-item active">
-            {/* <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg> */}
+          <div className="carousel-item active">
+            {/* <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg> */}
             <img
               className="d-block"
               src="https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png"
               height='300'
             />
-            <div class="container">
-              <div class="carousel-caption">
+            <div className="container">
+              <div className="carousel-caption text-black">
                 <h1>Events</h1>
-                <p>Find your next adventure or create your own!</p>
-                <p><a className="btn btn-outline-light btn-lg rounded-pill mb-3" href="/events/" role="button">See All Events</a></p>
+                <h3>Find your next adventure or create your own!</h3>
+                <div className='mt-4'>
+                  <p><a className="btn btn-outline-dark btn-lg rounded-pill mb-3" href="/events/" role="button">See All Events</a></p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+        <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+        <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
 
