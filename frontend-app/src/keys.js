2cbdb345ec251f095ec0d52fe9fbac9c3bf1bac5
Totz96 <lotztim@gmail.com>
2022-09-16 17:52:05 -0400

    hiding google api

--- frontend-app/src/keys.js
+++ frontend-app/src/keys.js
@@ -1,5 +1,5 @@
 export default function googleAPI() {
-  const REACT_APP_GOOGLE_API_KEY = "AIzaSyCikgdlt4Cso4GdFZYzgkKnGdptkbXsubw"
+  const REACT_APP_GOOGLE_API_KEY = `${process.env.REACT_APP_GOOGLE_API_KEY}`
   return (
     REACT_APP_GOOGLE_API_KEY
   )
